5319205f85201ca42f514c041a7fe0e7fdbfc08f
slagga <957766762@qq.com>
2019-07-29 13:32:27 +0800

    Create print.go

---
+++ go-cookbook/print.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	s := fmt.Sprintf("my name is %s", "slagga")
+	// Coroutine example
+	// print(s)
+	println(s)
+}
