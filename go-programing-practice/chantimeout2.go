1014c11d5d2fc91149a81838f4a3656563dcc396
slagga <957766762@qq.com>
2019-09-07 18:24:35 +0800

    add chan time files

---
+++ go-programing-practice/chantimeout2.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	intChan := make(chan int, 1)
+	go func() {
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Second)
+			intChan <- i
+		}
+		close(intChan)
+	}()
+	timeout := time.Millisecond * 500
+	var timer *time.Timer
+	for {
+		if timer == nil {
+			timer = time.NewTimer(timeout)
+		} else {
+			timer.Reset(timeout)
+		}
+		select {
+		case e, ok := <-intChan:
+			if !ok {
+				fmt.Println("End.")
+				return
+			}
+			fmt.Printf("Received: %v\n", e)
+		case <-timer.C:
+			fmt.Println("Timeout!")
+		}
+	}
+}
+
+//Timeout!
+//Timeout!
+//Received: 0
+//Timeout!
+//Timeout!
+//Received: 1
+//Timeout!
+//Timeout!
+//Received: 2
+//Timeout!
+//Received: 3
+//Timeout!
+//Received: 4
+//End.
