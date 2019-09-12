7967a5426565ed0efef5a6ca0ac73ab4b7d4ad18
slagga <957766762@qq.com>
2019-09-13 00:22:38 +0800

    add sync mutex files

---
+++ go-programing-practice/copiedvalue.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+func main() {
+	var countVal atomic.Value
+	countVal.Store([]int{1, 3, 5, 7})
+	anotherStore(&countVal)
+	fmt.Printf("The count value: %+v \n", countVal.Load())
+}
+
+func anotherStore(countVal *atomic.Value) {
+	countVal.Store([]int{2, 4, 6, 8})
+}
+
+//The count value: [2 4 6 8]
