82f001025003bc6e354c85ca575f397017e796e0
slagga <957766762@qq.com>
2019-10-10 00:43:54 +0800

    add basic testing and example testing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# go-rust-example
-go/rust example code
+## go-rust-example
+* go/rust example code
+
+## go doc usage
+* use command to show doc
+    * $ go doc tar
+* use a server to show basic doc list
+    * $ godoc http=:6060
---
+++ testing/Download_test.go
@@ -0,0 +1,67 @@
+package testing
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+const checkMark = "\u2713"
+const ballotX = "\u2717"
+
+func TestDownload(t *testing.T) {
+	url := "http://www.goinggo.net/index.xml"
+	statusCode := 200
+
+	t.Log("Given the need to test downloading content")
+	{
+		t.Logf("\tWhen checking \"%s\" for statuc code \"%d\"", url, statusCode)
+		{
+			resp, err := http.Get(url)
+			if err != nil {
+				t.Fatal("\t\tShould be able to make the Get call.", ballotX, err)
+			}
+
+			t.Log("\t\tShould be able to make the Get call.", checkMark)
+
+			defer resp.Body.Close()
+
+			if resp.StatusCode == statusCode {
+				t.Logf("\t\tShould receive a \"%d\" status. %v", statusCode, checkMark)
+			} else {
+				t.Logf("\t\tShould receive a \"%d\" status. %v %v", statusCode, ballotX, resp.StatusCode)
+			}
+		}
+	}
+}
+
+func ExampleRequest() {
+
+	url := "https://oapi.dingtalk.com/"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	type DingData struct {
+		ErrCode int    `json:"errcode"`
+		ErrMsg  string `json:"errmsg"`
+	}
+
+	var data DingData
+	err := json.Unmarshal([]byte(body), &data)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Println(data)
+	// Output:
+	// {404 请求的URI地址不存在}
+}
+
+// $ go test -v
+// $ go test -v -run="ExampleRequest"
