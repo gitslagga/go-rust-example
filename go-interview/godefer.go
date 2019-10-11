26aca390be3fe586345611c3116c7425e937876c
slagga <957766762@qq.com>
2019-10-11 11:05:49 +0800

    add goruntine file

--- go-interview/godefer.go
+++ go-interview/godefer.go
@@ -2,22 +2,50 @@ package main
 
 import "fmt"
 
-func calc(index string, a, b int) int {
-	ret := a + b
-	fmt.Println(index, a, b, ret)
-	return ret
-}
-
 func main() {
+	fmt.Println(DeferFunc1(1)) // 4
+	fmt.Println(DeferFunc2(1)) // 1
+	fmt.Println(DeferFunc3(1)) // 3
+
 	a := 1
 	b := 2
 	defer calc("1", a, calc("10", a, b))
 	a = 0
 	defer calc("2", a, calc("20", a, b))
 	b = 1
+
+	//10 1 2 3
+	//20 0 2 2
+	//2 0 2 2
+	//1 1 3 4
+
 }
 
-//10 1 2 3
-//20 0 2 2
-//2 0 2 2
-//1 1 3 4
+func DeferFunc1(i int) (t int) {
+	t = i
+	defer func() {
+		t += 3
+	}()
+	return t
+}
+
+func DeferFunc2(i int) int {
+	t := i
+	defer func() {
+		t += 3
+	}()
+	return t
+}
+
+func DeferFunc3(i int) (t int) {
+	defer func() {
+		t += i
+	}()
+	return 2
+}
+
+func calc(index string, a, b int) int {
+	ret := a + b
+	fmt.Println(index, a, b, ret)
+	return ret
+}
