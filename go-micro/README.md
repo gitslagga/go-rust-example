0851c4b620a4d734c9b79752f227a048a241cfe5
slagga <957766762@qq.com>
2021-04-22 10:01:15 +0800

    add grpc routeguide

--- go-micro/README.md
+++ go-micro/README.md
@@ -14,3 +14,11 @@ go get google.golang.org/protobuf/cmd/protoc-gen-go
 go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/helloworld/helloworld.proto
+
+## grpc routeguide
+
+go get google.golang.org/protobuf/cmd/protoc-gen-go
+
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/routeguide/route_guide.proto
--- go-micro/test/helloworld_test.go
+++ go-micro/test/helloworld_test.go
@@ -16,12 +16,12 @@ const (
 )
 
 // server is used to implement helloworld.GreeterServer.
-type grpcServer struct {
+type helloServer struct {
 	pb.UnimplementedGreeterServer
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *grpcServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+func (s *helloServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
@@ -32,7 +32,7 @@ func TestHelloWorldServer(t *testing.T) {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &grpcServer{})
+	pb.RegisterGreeterServer(s, &helloServer{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
