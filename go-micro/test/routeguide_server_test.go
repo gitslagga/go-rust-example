aab591878797ddff4dc59fde3bbcea1fca4aeb9c
slagga <957766762@qq.com>
2021-04-22 10:17:21 +0800

    add route_guide_db.json

--- go-micro/test/routeguide_server_test.go
+++ go-micro/test/routeguide_server_test.go
@@ -26,7 +26,7 @@ var (
 	tls        = flag.Bool("tls", true, "Connection uses TLS if true, else plain TCP")
 	certFile   = flag.String("cert_file", "", "The TLS cert file")
 	keyFile    = flag.String("key_file", "", "The TLS key file")
-	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	jsonDBFile = flag.String("json_db_file", "../data/testdata/route_guide_db.json", "A json file containing a list of features")
 	port       = flag.Int("port", 10000, "The server port")
 )
 
